8691ac573d7e5ce687c068db60c1ef1c6810cac3
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2017-03-25 09:18:40 +0200

    use urllib to get hw list from openwrt.org

--- openwrt_supported_hw_parser.py
+++ openwrt_supported_hw_parser.py
@@ -1,19 +1,17 @@
 #!/usr/local/bin/python3
-import subprocess
 import os
 from bs4 import BeautifulSoup
+import urllib.request
 
-with open("Table of Hardware [OpenWrt Wiki].html") as f:
-    data = f.read()
+with urllib.request.urlopen('https://wiki.openwrt.org/toh/start') as response:
+    data = response.read()
     soup = BeautifulSoup(data, "html.parser")
- 
 
-
-for brand, model  in zip(
-    soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(3) '), 
-    soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(2)')):
-    cmd = "python3 /Users/mikko/ohjelmointi/github/vahti/vahti.py -p tori -q '{0} {1}'".format(model.text, brand.text)
-    print(cmd)
-    os.system(cmd)
-    #subprocess.call(cmd)
-    #print("{0} {1}".format(model.text, brand.text)) 
+    for brand, model  in zip(
+        soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(3) '), 
+        soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(2)')
+        ):
+        print("test")
+        cmd = "python3 /Users/mikko/ohjelmointi/github/vahti/vahti.py -p tori -q '{0} {1}'".format(model.text, brand.text)
+        print(cmd)
+        os.system(cmd)
