dadce46d23f0b2cf3e9b2d430fc76cf01b5bab43
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2019-03-03 17:22:08 +0200

    stupid me

--- openwrt_supported_hw_parser.py
+++ openwrt_supported_hw_parser.py
@@ -1,19 +1,20 @@
 #!/usr/local/bin/python3
-import subprocess
 import os
 from bs4 import BeautifulSoup
+import requests
 
-with open("Table of Hardware [OpenWrt Wiki].html") as f:
-    data = f.read()
-    soup = BeautifulSoup(data, "html.parser")
- 
+response = requests.get("https://wiki.openwrt.org/toh/start")
+if response.status_code == 200:
+    #print("requests")
+    #print(response.content)
+    soup = BeautifulSoup(response.content, "html.parser")
+    zipped = zip(
+        soup.select('.brand'), 
+        soup.select('.model'))
 
+    for brand, model in zipped:
+        cmd = "python3 /Users/mikko/ohjelmointi/github/vahti/vahti.py -p tori -q '{0} {1}'".format(model.text, brand.text)
+        print(cmd)
+        os.system(cmd)
 
-for brand, model  in zip(
-    soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(3) '), 
-    soup.select('#dokuwiki__content > div > div.page.group > div > div.table.dataaggregation > table > tbody > tr > td:nth-of-type(2)')):
-    cmd = "python3 /Users/mikko/ohjelmointi/github/vahti/vahti.py -p tori -q '{0} {1}'".format(model.text, brand.text)
-    print(cmd)
-    os.system(cmd)
-    #subprocess.call(cmd)
-    #print("{0} {1}".format(model.text, brand.text)) 
+    
